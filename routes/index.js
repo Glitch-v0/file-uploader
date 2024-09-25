601ea1c2c4e9f8d48b76c06d0c24ddf066f6043f
Nathan Jones <46408104+Glitch-v0@users.noreply.github.com>
2024-09-25 14:07:41 -0500

    Add basic file upload functionality

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,17 @@ const passport = require("passport");
 const genPassword = require("../lib/passwordUtils").genPassword;
 // const { body, validationResult } = require("express-validator");
 // const { nameValidationRules } = require("../validators/nameValidators");
+const multer = require("multer");
+const upload = multer({ dest: "./uploads/" });
 
 router.get("/", (req, res, next) => {
   res.render("index");
+});
+
+router.post("/", upload.single("file-upload"), (req, res, next) => {
+  console.log(req.file);
+  res.render("index");
+  console.log(`File saved to ${req.file.path}`);
 });
 
 module.exports = router;
